fb2336dd057e0025c471b42ee64a6828137233b7
Somu-techy <berasoumyadipta@gmail.com>
2025-05-24 14:56:45 +0000

    variables

---
+++ variables.js
@@ -0,0 +1,12 @@
+const accountId=144678
+let accountEmail= "somu@google.com"
+var accountPassword="12345"// we dont use this now, this is wrong
+accountCity="Jaipur"// dont use this method
+let accountState;
+
+// accountId=2 // not allowed, value is constant
+accountEmail="hc@hc.com"
+console.log(accountId);
+accountCity="Kolkata"
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
