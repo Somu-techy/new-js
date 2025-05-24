2f5407bca8c558b4f3810e64cb231be9c94725b7
Somu-techy <berasoumyadipta@gmail.com>
2025-05-24 14:51:48 +0000

    datatypes

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all JS code as newer version
+// alert(3+3)// we are using nodejs, not browser
+
+let name="Somu"// string
+let age=21// number
+let isLoggedIn= false// boolean
+let state = null
+
+
+// number => 2 to power of 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+
+console.log(typeof "Somu");
+console.log(typeof null);
+console.log(typeof undefined);
