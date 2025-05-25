91c597cedeaac2caccac972491e3ce5b44b94d29
Somu-techy <berasoumyadipta@gmail.com>
2025-05-25 16:21:47 +0000

    conversionOperations

--- conversionOperations.js
+++ conversionOperations.js
@@ -34,3 +34,42 @@ let someNum = 33
 
 let StringNumber = String(someNum)
 console.log( typeof StringNumber)
+
+// ########################################### Operations ########################################################################
+
+ let value = 3
+ let negVal= -value
+ console.log(negVal);
+ 
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2*2);
+//  console.log(2**2);
+//  console.log(2/2);
+//  console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Somu"
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+ 2)
+console.log(1 + "2")
+console.log("1"+ 2 + 2) // rule or guidelines
+console.log(1 + 2 + "2"); // rule or guidelines 
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3 
+num1= num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
