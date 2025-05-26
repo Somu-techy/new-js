9e16fd26078847f18d396877754ceb2484f4c770
Somu-techy <berasoumyadipta@gmail.com>
2025-05-26 12:21:27 +0000

    datatypes summary

---
+++ datatypes-summary.js
@@ -0,0 +1,54 @@
+// Primitive
+// 7 types : String , Number , Boolean, null, undefined, Symbo ,BigInt
+
+const score= 100
+const scoreValue= 100.3
+
+const isLoggedin = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId)
+
+let bigNumber = 84784583010231028347568946744656483n
+
+
+// Reference (Non Primitive)
+
+// Array , Objects, Functions
+
+const heroes =[ "shaktiman", "naagraj", "doga"]
+
+{
+    name : "Somu"
+    age : 21
+}
+
+let myObj = {
+     name : "Somu",
+    age : 21,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
+console.log(typeof null);//=> object
+console.log(typeof bigNumber);//=> number
+
+//return type of variables in Javascript
+
+// Number=>number
+// String=>string
+// Boolean=> boolean
+// null=> object
+// undefined=>undefined
+// Symbol=> symbol
+// BigInt=> bigint
+
+// Arrays => object
+// Function => function
+// Object => object
