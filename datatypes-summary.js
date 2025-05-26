c9b55e982de216c44b20751a963153afa3674393
Somu-techy <berasoumyadipta@gmail.com>
2025-05-26 14:26:12 +0000

    datatypes summary & stack and heap

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -52,3 +52,28 @@ console.log(typeof bigNumber);//=> number
 // Arrays => object
 // Function => function
 // Object => object
+
+// ################################
+
+// Stack(primitive), heap(non-primitive)
+
+let myYoutubename = "somuchannel"
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "somu@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
