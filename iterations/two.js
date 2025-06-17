246f49b27d3bf785ff564ff9b6aa6bf5b628aa90
Somu-techy <berasoumyadipta@gmail.com>
2025-06-17 16:48:37 +0000

    while loop and do-while loop

---
+++ iterations/two.js
@@ -0,0 +1,24 @@
+// while loop
+
+let i = 0
+while (i <= 10) {
+    console.log(`Value of insex is ${i}`);
+    i=i+2
+}
+
+
+let myArray =['flash', 'batman', 'superhuman']
+
+let arr = 0
+while (arr < myArray.length){
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr+1
+    
+}
+
+let score = 12
+// do while loop
+do {
+    console.log(`Score is ${score}`);
+    score++
+}while(score <= 10)
